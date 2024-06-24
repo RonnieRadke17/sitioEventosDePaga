2114ee115ff56224b717509d494249836480bff4
IvanVL-18 <120744978+IvanVL-18@users.noreply.github.com>
2024-06-24 16:27:37 -0600

    cambio en HOME

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel - PayPal Integration</title>
+    <title>Laravel - PayPal Integration Hola</title>
 </head>
 <body>
 
