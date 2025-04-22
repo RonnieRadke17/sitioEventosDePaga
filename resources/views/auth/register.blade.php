a762a12ae1564831ea1bf1c447b535cd6b2be0ef
RonnieRadke17 <alexisemmanuelzombie@gmail.com>
2025-04-22 00:32:54 -0600

    modificaciones menores en el form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,40 +41,11 @@
                 <!-- Nombre -->
                 <x-forms.input-text class="col-span-1 md:col-span-2" name="name" description="Nombre"></x-forms.input-text>
                 
-                {{-- <div class="col-span-1 md:col-span-2">
-                    <div class="relative">
-                        <input type="text" name="name" id="name" autocomplete="off" value="{{ old('name') }}" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                        <label for="name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Nombres</label>
-                    </div>
-                    @error('name')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
-                    @enderror
-                </div> --}}
-
                 <!-- Apellido Paterno -->
                 <x-forms.input-text name="paternal" description="Apellido Paterno"></x-forms.input-text>
-                
-                {{-- <div>
-                    <div class="relative">
-                        <input type="text" name="paternal" id="paternal" autocomplete="off" value="{{ old('paternal') }}" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                        <label for="paternal" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Apellido Paterno</label>
-                    </div>
-                    @error('paternal')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
-                    @enderror
-                </div> --}}
 
                 <!-- Apellido Materno -->
                 <x-forms.input-text name="maternal" description="Apellido Materno"></x-forms.input-text>
-                {{-- <div>
-                    <div class="relative">
-                        <input type="text" name="maternal" id="maternal" autocomplete="off" value="{{ old('maternal') }}" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                        <label for="maternal" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Apellido Materno</label>
-                    </div>
-                    @error('maternal')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
-                    @enderror
-                </div> --}}
 
                 <!-- Fecha de nacimiento -->
                 <div class="md:col-span-2">
@@ -103,15 +74,6 @@
 
                 <!-- Correo Electrónico -->
                 <x-forms.input-text class="col-span-1 md:col-span-2" name="email" description="Correo"></x-forms.input-text>
-                {{-- <div class="col-span-1 md:col-span-2">
-                    <div class="relative">
-                        <input type="email" name="email" id="email" autocomplete="off" value="{{ old('email') }}" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                        <label for="email" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Correo</label>
-                    </div>
-                    @error('email')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
-                    @enderror
-                </div> --}}
 
                 <!-- Contraseña -->
                 <div class="relative">
--- resources/views/event/form.blade.php
+++ resources/views/event/form.blade.php
@@ -79,8 +79,8 @@
     {{-- capacity --}}
     <div class="mb-4">
         <div class="relative">
-            <input type="text"  name="capacity" id="capacity" autocomplete="off" value="{{ old('capacity', $event->capacity ?? '') }}"  class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
-            <label for="name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Capacidad(Opcional)</label>
+            <input type="number"  name="capacity" id="capacity" autocomplete="off" value="{{ old('capacity', $event->capacity ?? '') }}"  class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
+            <label for="capacity" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Capacidad(Opcional)</label>
         </div>
         @error('capacity')
             <span class="text-red-500 text-sm">{{ $message }}</span>
@@ -90,7 +90,7 @@
     <div class="mb-4">
         <div class="relative">
             <input type="text"  name="price" id="price" autocomplete="off" value="{{ old('price', $event->price ?? '') }}"  class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
-            <label for="name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Precio</label>
+            <label for="price" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Precio</label>
         </div>
         @error('price')
             <span class="text-red-500 text-sm">{{ $message }}</span>
